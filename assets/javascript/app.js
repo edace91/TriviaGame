8613bbbf578c5f35695423a02551719f9143feac
edace91 <eduace91@gmail.com>
2016-09-02 19:12:47 -0500

    Structured the aswer key object to begin recognizing using input as correct, incorrect or unanswered

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,8 @@
 
     // make variables global to the runtime of our application
     var audio, timer, time, correctAnswers, incorrectAnswers, unanswered;
-    //hide the quiz until user clocks on the start button
     var audio = new Audio('https://p.scdn.co/mp3-preview/ed5a443bc86176135ebca8a114f66f4d814d4c90');
-    
+    //hide the quiz until user clocks on the start button
     $("#quiz").hide();
     $("#done").hide();
     
@@ -13,12 +12,64 @@
         	$("#quiz").show();
             timer.start();
             audio.play();
+            setTimeout(timeUp, 1000 * 80);
             //make the timer start 1:20 seconds by recalling function 
     });
     //play music while playing trivia game
     
+    $('input[type="radio"]:checked').each(function() {
 
-    setTimeout(timeUp, 1000 * 82);
+        if (this.value > 0 && this.value <= 2) {
+           // do something if the value is less than zero and below or equal to two
+        }else if (this.value > 2) {
+           // do something else if the value is greater than two
+        }
+    });
+
+    //create an object answer key, with key objects for each individual 
+   //question, Question 1 should be something specific and the value should 
+   //be the question answer. and name it the answer then do a checker to 
+   // compare the radio clicked value to the answer.
+   var answerKey =
+   {
+        "#questionFoundation":
+        {
+            "wrongYear1": "1986",
+            "correctYear": "1889",
+            "wrongYear2": "1973",
+            "wrongYear2": "1968",
+        }
+        "#questionMiyamoto":
+        {
+            "worked1": "Mario",
+            "worked2": "The Legend of Zelda",
+            "worked3": "Donkey Kong", 
+            "didntWork": "pokemon",
+        }
+        "#questionZelda":
+        {
+            "wrongLocation1": "The Water Temple",
+            "correctLocation": "The Graveyard",
+            "wrongLocation2": "Kokiri Forest",
+            "wrongLocation3": "Lord Jabu-Jabu's Belly",
+        }
+        "#questionMarioSport":
+        {
+            "realSport1": "Mario Hoops 3-on-3",
+            "realSport2": "Mario Golf",
+            "realSport3": "Super Mario Strikers",
+            "fakeSport": "Super Mario Boxing",
+        }
+        "#questionNext":
+        {
+            "fakeConsole1": "Wii 2",
+            "fakeConsole2": "Nin Ex",
+            "realConsole": "NX",
+            "fakeConsole3": "Wii X",
+        }
+   }
+
+
 
     //timer object
     var timer = {
@@ -40,7 +91,7 @@
             // Get the current time, pass that into the stopwatch.timeConverter function, 
             // and save the result in a variable.
             var converted = timer.timeConverter(timer.time);
-            //Use the variable you just created to show the converted time in the "display" div.
+            // Use the variable you just created to show the converted time in the "display" div.
             $('#display').html(converted);
         },
 
@@ -61,7 +112,7 @@
 
     function timeUp(){
 
-    // Once 80 seconds pass, time is up! 
+    // Once 80 seconds pass, time is up! hide quiz display results.
         $("#quiz").hide();
         $("#done").show();
 
